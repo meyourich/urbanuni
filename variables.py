3f9d9e894aae543b94e6610287f959ae24dd0ef2
meyourich <meyourich@ya.ru>
2025-01-13 22:12:13 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+one_task_time = hours_spent / completed_tasks
+print('Курс:', course_name + ', всего задач:', str(completed_tasks) + ', затрачено часов: ' + str(hours_spent) + ', среднее время выполнения', str(one_task_time), 'часа.')
